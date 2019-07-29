662f749799a3bd44c693126490d9c3d91a8938f8
hyunji1994 <guswl5059@naver.com>
2019-07-29 14:04:20 +0900

    날짜 링크 없앰

--- src/main/java/schedule/CalendarUtil.java
+++ src/main/java/schedule/CalendarUtil.java
@@ -18,12 +18,24 @@ public class CalendarUtil {
 	}
 	
 	// 날짜를 클릭하면 그날 일정이 모두 보이는 caldetail로 이동
+	
+//	public String caldetail(int year, int month, int day) {
+//		String s = "";
+//		s += String.format("<a href='%s?year=%d&month=%d&day=%d'>",
+//				"/schedule/caldetail", year, month, day);
+//		s += String.format("%2d", day); //2자리
+//		s += "</a>";
+//		
+//		return s;
+//	}
+	
 	public String caldetail(int year, int month, int day) {
 		String s = "";
-		s += String.format("<a href='%s?year=%d&month=%d&day=%d'>",
-				"/schedule/caldetail", year, month, day);
+		String url = "/schedule/caldetail";
+		
+		s = String.format("<a href='%s?year=%d&month=%d&day=%d'></a>", 
+				url, year, month, day);
 		s += String.format("%2d", day); //2자리
-		s += "</a>";
 		
 		return s;
 	}
@@ -33,7 +45,7 @@ public class CalendarUtil {
 		String s = "";
 		
 		String url="/schedule/calwrite";
-		String image = "<img src='/resources/img/pen.gif'>";
+		String image = "<img src='/resources/img/pen.jpg'>";
 		s = String.format("<a href='%s?year=%d&month=%d&day=%d'>%s</a>", 
 				url, year, month, day, image);
 		
@@ -67,7 +79,7 @@ public class CalendarUtil {
 		for(CalendarDto dto : list) {
 			if(dto.getRdate().substring(0,8).equals(dates)) {
 				
-				s += "<tr bgcolor='#FFCA9B'>";
+				s += "<tr bgcolor='#FFE4B5'>";
 				s += "<td>";
 				s += "<a href='caldetail?calendar_idx=" + dto.getCalendar_idx() + "'>";
 				s += "<font style='font-size:15; color:black'>";
@@ -76,7 +88,7 @@ public class CalendarUtil {
 				s += "</a>";
 				s += "</td>";
 				s += "</tr>";
-				s += "<tr height=\"10px\"><td></td></tr>";
+				s += "<tr height=\"5px\"><td></td></tr>";
 				
 			}
 		}
@@ -90,8 +102,8 @@ public class CalendarUtil {
 	// 일정제목이 길면 ...처리
 	public String dot3(String msg) {
 		String s = "";
-		if(msg.length() >= 10) {
-			s = msg.substring(0,10); //임의로 10글자 까지
+		if(msg.length() >= 15) {
+			s = msg.substring(0,15); //임의로 10글자 까지
 			s += "...";
 		} else {
 			s = msg.trim();
