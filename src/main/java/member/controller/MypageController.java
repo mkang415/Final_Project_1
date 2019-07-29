fefcfdb4534980f3c4de07dfb008f9995e21f577
hyunji1994 <guswl5059@naver.com>
2019-07-29 14:05:25 +0900

    마이페이지 만드는 중

---
+++ src/main/java/member/controller/MypageController.java
@@ -0,0 +1,16 @@
+package member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MypageController {
+
+	
+	//마이페이지
+	@RequestMapping(value = "/member/mypage", method = RequestMethod.GET)
+	public void mypage ( ) {
+		
+	}
+}
